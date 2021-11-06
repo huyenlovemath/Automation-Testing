9f9c5112d921fa9f15c6dce5a3c6c0f015fb7f03
huyenlovemath <57594882+huyenlovemath@users.noreply.github.com>
2021-11-06 15:13:09 +0700

    add Switch to night theme

--- src/test/java/Appiumtests/Account/ChangeTheme.java
+++ src/test/java/Appiumtests/Account/ChangeTheme.java
@@ -1,6 +1,7 @@
 package Appiumtests.Account;
 
 import Appiumtests.MobileTest;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -13,16 +14,28 @@ import java.util.concurrent.TimeUnit;
 public class ChangeTheme extends MobileTest {
     public ChangeTheme() throws IOException {
     }
+
+    private static final String dayTheme = "//android.widget.ImageView[@text='Switch to night theme']";
+    private static final String nightTheme  = "//android.widget.ImageView[@text='Switch to day theme']";
     @Test
     public void ChangeUserTheme() {
         System.out.println(" Start Change Theme test");
 
         WebElement Menu = driver.findElementByXPath("//android.widget.ImageView[@content-desc='Open navigation menu']");
         Menu.click();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        WebElement changeTheme = driver.findElementByXPath("//android.widget.ImageView[@text='Switch to day theme']");
-        changeTheme.click();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        WebElement changeTheme;
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        try {
+            changeTheme = driver.findElementByXPath(dayTheme);
+            changeTheme.click();
+        } catch (NoSuchElementException e) {
+            changeTheme = driver.findElementByXPath(nightTheme);
+            changeTheme.click();
+        }
+
+
+
 
     }
 }
